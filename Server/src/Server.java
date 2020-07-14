363b9541690480b891adafaea533e607dc12c194
VitorAllux <vitorpbemp@gmail.com>
2020-07-14 19:45:20 -0300

    Server feito basico

---
+++ Server/src/Server.java
@@ -0,0 +1,71 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+	
+	private ServerSocket serverSocket;
+	
+	
+	//Criar o server socket
+	private void createServerSocket(int port) throws IOException {
+			serverSocket = new ServerSocket(port);
+	}
+	
+	//Esperar a conexão com o "cliente", retorna o socket que recebeu a conexão
+	private Socket waitConnectionSocket() throws IOException {
+		Socket socket = serverSocket.accept();
+		return socket;
+	}
+	
+	//Fechar Socket
+	private void closeSocket(Socket socket) throws IOException {
+		socket.close();
+	}
+	
+	private void processConnection(Socket socket) throws IOException {
+		//"cliente" ----> socket <---- "server"
+		try {
+			ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
+			ObjectInputStream input = new ObjectInputStream(socket.getInputStream()); 
+		
+			//protocol
+			String msg = input.readUTF();
+			System.out.println("recebeu msg");
+			output.writeUTF("Hello World!");
+			
+			output.close();
+			input.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally {
+			closeSocket(socket);
+		}
+
+
+		
+	}
+	
+	public static void main(String[] args) {
+		try {
+			Server server = new Server();
+			System.out.println("waiting connection!..");
+			server.createServerSocket(55555);
+			Socket socket = server.waitConnectionSocket();//protocol
+			System.out.println("connected!");
+			server.processConnection(socket);//trata conexão
+			System.out.println("connection closed!");
+		} catch (IOException e) {
+			//trata exception
+		}
+
+
+	}
+	
+	
+
+}
